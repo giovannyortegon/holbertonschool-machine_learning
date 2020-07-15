aa09df1ad76853fd29834a9beae47fb8d66a99c6
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-15 14:34:24 -0500

    [ADD] size matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,14 +4,16 @@ import numpy as np
 
 
 def matrix_shape(matrix):
-    """ matrix_shape
-
-        matrix: Recive a list
-        Return: A list with the dimensions
+    """ matrix_shape function - calculates the shape of a matrix
+    matrix: inout matrix
+    Return: size of matrix
     """
+    new_list = []
     if len(matrix) == 0:
         return [0]
-    elif type(matrix[0]) is not list:
-        return [len(matrix)]
     else:
-        return [len(matrix)] + matrix_shape(matrix[0])
+        arr = np.array(matrix)
+        sh = arr.shape
+        for i in range(0, len(sh)):
+            new_list.append(sh[i])
+    return new_list
