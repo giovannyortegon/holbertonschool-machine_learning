4316de9a2867fe8d2d81d87a589c930b4546e87c
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-16 18:43:54 -0500

    [FIX] return None

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,13 +9,16 @@ def cat_matrices2D(mat1, mat2, axis=0):
     mat2: Input second matrix
     Return: New matrix
     """
-    new_mat = []
-    a = mat1[:]
-    b = mat2[:]
-    if axis == 1:
-        for i in range(len(mat1[0])):
-            new_mat.append(cat_arrays(a[i], b[i]))
-        return new_mat
-    else:
-        new_mat = a + b
-        return new_mat
+    try:
+        new_mat = []
+        a = mat1[:]
+        b = mat2[:]
+        if axis == 1:
+            for i in range(len(mat1[0])):
+                new_mat.append(cat_arrays(a[i], b[i]))
+            return new_mat
+        else:
+            new_mat = a + b
+            return new_mat
+    except Exception:
+        return None
