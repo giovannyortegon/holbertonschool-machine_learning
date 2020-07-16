0d5a03f460b81694fb27533f842af21ee932da0a
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-16 01:36:38 -0500

    [ADD] howdy

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Concatenates two arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ cat_arrays
+    arr1: First matrix
+    arr2: Second Matrix
+    Return: Result of elements of matrix
+    """
+    return arr1 + arr2
