9403d3dea9e0f6c9fa487a8d6c1be4080dabbc9f
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-16 23:26:16 -0500

    [ADD] concatenate

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" concatenate two matrices along a specific axis """
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ np_cat - concatenate two matrices
+    mat1: First matrix
+    mat2: Second matrix
+    axisi: along of a matrix
+    Return:  a new numpy.ndarray
+    """
+    arr = np.concatenate((mat1, mat2), axis=axis)
+    print(type(arr))
+    return arr
