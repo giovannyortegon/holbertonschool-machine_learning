eb102ba5946782e5d3dcb0ad9a0958f2d2c6de47
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-16 22:47:27 -0500

    [ADD] transpose array

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Transposes of a matrix """
+
+
+def np_transpose(matrix):
+    """ np_transpose - Transposes of a matrix
+    matrix: np.array
+    Return: return a new numpy.ndarray
+    """
+    return matrix.T
