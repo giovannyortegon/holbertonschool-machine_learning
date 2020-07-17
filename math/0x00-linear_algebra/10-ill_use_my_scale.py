35f257f34dc13a0438dda681c1c2825c3f167950
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-16 22:39:27 -0500

    [ADD] shape a matrix

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" calculates the shape of a matrix """
+
+
+def np_shape(matrix):
+    """ np_shape - calculates the shape of a matrix
+    matrix: np.array
+    Return: a shape as a tuple of integers
+    """
+    return matrix.shape
