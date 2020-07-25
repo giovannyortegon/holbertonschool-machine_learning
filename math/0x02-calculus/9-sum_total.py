a9085b7554b800cb03c494777a1126e663843111
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-24 20:51:52 -0500

    [ADD] Sigma Notation

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -10,10 +10,10 @@ def summation_i_squared(n):
         Return:
             res:(int) Sigma Notation of a number
     """
-    if type(n) is int:
-        res = 0
-        for i in range(1, n + 1):
-            res += i**2
-        return res
-    else:
+    if type(n) is not int:
         return None
+    else:
+        if n == 1:
+            return 1
+        else:
+            return summation_i_squared(n - 1) + (n ** 2)
