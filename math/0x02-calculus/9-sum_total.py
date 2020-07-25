09aec7eb453095dd6f4cf1ef7743001d48be91ea
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-24 22:04:22 -0500

    ]

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -16,4 +16,4 @@ def summation_i_squared(n):
         if n == 1:
             return 1
         else:
-            return summation_i_squared(n - 1) + (n ** 2)
+            return int(n * (n + 1) * (2 * n + 1)/6)
