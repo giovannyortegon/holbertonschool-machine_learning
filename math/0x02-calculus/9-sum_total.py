346541fdd075948e675655ab257cc3ed303aeb07
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-24 22:06:54 -0500

    [FIX] decition None

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -13,7 +13,4 @@ def summation_i_squared(n):
     if type(n) is not int or n is None:
         return None
     else:
-        if n == 1:
-            return 1
-        else:
-            return int(n * (n + 1) * (2 * n + 1)/6)
+        return int(n * (n + 1) * (2 * n + 1)/6)
