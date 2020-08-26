77fcc476d05a7a9a203c43cef1746a071935289a
Skillhh <ortegon.giovanny@hotmail.com>
2020-08-25 19:21:43 -0500

    [ADD] momentum update

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" create momentum """
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """ create_momentum_op - updates a variable using
+        the gradient descent with momentum optimization algorithm.
+
+    Args:
+        loss is the loss of the network
+        alpha is the learning rate
+        beta1 is the momentum weight
+
+    Returns:
+        the momentum optimization operation
+    """
+    optimize = tf.train.MomentumOptimizer(alpha, beta1)
+
+    return optimize.minimize(loss)
