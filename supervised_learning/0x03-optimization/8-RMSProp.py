0391fa40647031f212035c2bfdceac98df7225c3
Skillhh <ortegon.giovanny@hotmail.com>
2020-08-27 22:17:16 -0500

    [ADD] RMSProp

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" RMSProp optimization algorithm """
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ create_RMSProp_op
+
+    Args:
+        loss is the loss of the network
+        alpha is the learning rate
+        beta2 is the RMSProp weight
+        epsilon is a small number to avoid division by zero
+
+    Returns:
+        the RMSProp optimization operation
+    """
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2,
+                                          epsilon=epsilon)
+
+    train = optimizer.minimize(loss)
+
+    return train
